b79d023a2b71a5e181d7c6878a69c22741568c5b
Rob Watson <rwatso@gmail.com>
2020-09-26 10:21:57 -0700

    Indentation fix

--- activitySplitter.hpp
+++ activitySplitter.hpp
@@ -18,27 +18,25 @@
 
 class activitySplitter {
 public:
-	// each incoming object gets mapped to an 'act', creates a new set for each
-	// completeActivity object by inserting each into a map
-	static Activity_Map doSplit(std::vector<completeActivity> & activities) {
-		Activity_Map activity_map; // declare a variable of type Activity_Map
-		for (completeActivity act : activities) {
-			activity_map[act.getWalkName()].insert(act);
-		}
-		return activity_map;
-	}
+  // each incoming object gets mapped to an 'act', creates a new set for each
+  // completeActivity object by inserting each into a map
+  static Activity_Map doSplit(std::vector<completeActivity> & activities) {
+	  Activity_Map activity_map; // declare a variable of type Activity_Map
+	  for (completeActivity act : activities) {
+		  activity_map[act.getWalkName()].insert(act);
+	  }
+	  return activity_map;
+  }
 
-	static void dump(const Activity_Map & activity_map) {
-		// for each pair<act name, set of activities>
-		for (auto act : activity_map) {
-			std::cout << "Activity Name: " << act.first << " " << std::endl;
-				for (auto instance : act.second) {
-					std::cout << instance << std::endl;
-				}
-
-		}
-	}
-
+  static void dump(const Activity_Map & activity_map) {
+  // for each pair<act name, set of activities>
+     for (auto act : activity_map) {
+         std::cout << "Activity Name: " << act.first << " " << std::endl;
+	    for (auto instance : act.second) {
+		std::cout << instance << std::endl;
+	    }
+     }
+  }
 };
 
 #endif /* activitySplitter_hpp */
